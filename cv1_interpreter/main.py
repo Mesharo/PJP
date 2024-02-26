b4ceb55a3e2db8cc0030d15f3f0c71efd9b0d31b
Mesharo <Hecko97@seznam.cz>
2024-02-26 11:56:02 +0100

    check syntax, modification

--- cv1_interpreter/main.py
+++ cv1_interpreter/main.py
@@ -1,11 +1,29 @@
-def modify(expression : str) -> str:
-    pass
+def removeSpaces(expression : str) -> str:
+    return ("".join(expression.split(" ")))
+
+def isOperatorOrParenthesis(character : str) -> bool:
+    return (character in ("+", "-", "*", "/", "(", ")"))
+
+def invalidCharacters(modifiedExpression : str):
+    for character in modifiedExpression:
+        if not(character.isdigit() or isOperatorOrParenthesis(character)):
+            return True
+    return False
 
 def solvable(expression : str) -> bool:
+    modifiedExpression = removeSpaces(expression)
+
+    if (invalidCharacters(modifiedExpression)):
+        return False
+
+    stack = []
+
+    # TODO check parenthesis parity and no unary exps
+
     return True
 
 def solve(expression : str) -> None:
-    if solvable(expression):
+    if (solvable(expression)):
         print(eval(expression))
     else:
         print("ERROR")
