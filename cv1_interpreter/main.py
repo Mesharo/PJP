c27974685c117bc642b1158564b6a7c142dafe2a
Mesharo <Hecko97@seznam.cz>
2024-02-26 21:58:31 +0100

    parentheses done

--- cv1_interpreter/main.py
+++ cv1_interpreter/main.py
@@ -10,16 +10,52 @@ def invalidCharacters(modifiedExpression : str):
             return True
     return False
 
+def unbalancedParentheses(expression : str) -> bool:
+    balance = 0
+
+    for character in expression:
+        if (balance < 0):
+            return True
+        if (character == "("):
+            balance += 1
+        if (character == ")"):
+            balance -= 1
+    
+    if (balance != 0):
+        return True
+    return False
+
+def emptyParentheses(expression : str) -> bool:
+    for index, character in enumerate(expression):
+        if (character == "("):
+            if not(expression[index + 1].isdigit() or expression[index + 1] == "("):
+                return True
+    return False
+
+def wrongConnection(expression : str) -> bool:
+    pass
+
+def unaryOperators(expression : str) -> bool:
+    pass
+
 def solvable(expression : str) -> bool:
     modifiedExpression = removeSpaces(expression)
 
     if (invalidCharacters(modifiedExpression)):
         return False
 
-    stack = []
-
-    # TODO check parenthesis parity and no unary exps
-
+    if (unbalancedParentheses(modifiedExpression)):
+        return False
+    
+    if (emptyParentheses(modifiedExpression)):
+        return False
+    
+    if (wrongConnection(modifiedExpression)):
+        return False
+    
+    if (unaryOperators(modifiedExpression)):
+        return False
+    
     return True
 
 def solve(expression : str) -> None:
