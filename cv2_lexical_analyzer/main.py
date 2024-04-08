b4d75d09f0d2a2ee8eb0299c6d13a1b7e117c152
Mesharo <Hecko97@seznam.cz>
2024-04-08 18:14:46 +0200

    old, not done, irrelevant

--- cv2_lexical_analyzer/main.py
+++ cv2_lexical_analyzer/main.py
@@ -1,2 +1,36 @@
+def solve(expression : str) -> None:
+    tokens = {
+        "+": "OP",
+        "-": "OP",
+        "*": "OP",
+        "/": "OP",
+        "(": "LPAR",
+        ")": "RPAR",
+        ";": "SEMICOLON"
+    }
+
+    foundComment = False
+    foundWord = False
+    currentWord = ""
+
+    for index, character in enumerate(expression):
+        if (foundComment is True):
+            if (character == "\n"):
+                foundComment = False
+            continue
+
+        if (character.isdigit()):
+            print(f'NUM:{character}')
+            continue
+
+        if (character.isalpha()):
+            pass      
+
+        print(f'{tokens[character]}:{character}')
+
 if __name__ == "__main__":
-    print("Hello World!")
+    #solve(input())
+
+    xd = "abcdefedcba"
+
+    print(xd == xd[::-1])
